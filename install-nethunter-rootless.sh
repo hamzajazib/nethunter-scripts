2e08c17d88bacf89186561972eabf3066530d418
hamzajazib <82419998+hamzajazib@users.noreply.github.com>
2021-04-27 18:37:09 +0500

    Update install-nethunter-rootless.sh

--- install-nethunter-rootless.sh
+++ install-nethunter-rootless.sh
@@ -5,8 +5,8 @@
 #(1) Install the NetHunter-Store app from store.nethunter.com
 #(2) Install Termux, NetHunter-KeX client, and Hacker’s keyboard from the NetHunter-Store
 
-#termux-setup-storage
-pkg update && pkg install wget && pkg install termux-api
+termux-setup-storage
+pkg update && pkg install wget termux-api
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod +x install-nethunter-termux
 install-nethunter-termux
