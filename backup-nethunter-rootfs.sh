6d3c0ace761d5553e0c35108678d224e6e93f2fe
hamzajazib <82419998+hamzajazib@users.noreply.github.com>
2021-04-27 18:41:05 +0500

    Create backup-nethunter-rootfs.sh

---
+++ backup-nethunter-rootfs.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#Documentation @ https://www.kali.org/docs/nethunter/nethunter-rootless/
+tar -cJf kali-arm64.tar.xz kali-arm64 && mv kali-arm64.tar.xz storage/downloads
