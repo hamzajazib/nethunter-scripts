125293e53a6840a08f1e5f5c0084a9be2ac541e8
hamzajazib <82419998+hamzajazib@users.noreply.github.com>
2021-04-27 16:25:26 +0500

    Add files via upload

---
+++ install-nethunter-rootless.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#Documentation @ https://www.kali.org/docs/nethunter/nethunter-rootless/
+
+#(1) Install the NetHunter-Store app from store.nethunter.com
+#(2) Install Termux, NetHunter-KeX client, and Hacker’s keyboard from the NetHunter-Store
+
+termux-setup-storage
+pkg update && pkg install wget
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+install-nethunter-termux
